fe8f72068d7ff6387b7cb407997a941b4559fe48
Stefi-C <cristinistif000@icloud.com>
2024-06-27 10:42:26 +0200

    contact section setup

--- script.js
+++ script.js
@@ -31,3 +31,55 @@ function fadeOut(){
 window.onload = fadeOut();
 
  */
+
+      /*--------------------------------gallery*/
+
+    
+       
+      const images = [...document.querySelectorAll('.img')];
+
+      const popup = document.querySelector('.popup');
+      const closeBtn = document.querySelector('.close-btn');
+      const largeImage = document.querySelector('.large-image');
+      const imageIndex = document.querySelector('.index');
+      const leftArrow = document.querySelector('.left-arrow');
+      const rightArrow = document.querySelector('.right-arrow');
+
+
+      let index = 0;
+
+      images.forEach((item, i) =>{
+        item.addEventListener('click', ()=>{
+            popup.classList.toggle('active');
+            updateImage(i);
+           
+        })
+      })
+      const updateImage = (i) => {
+        let path = `/assets/gallery${i+1}.jpeg`;
+        largeImage.src = path;
+        imageIndex.innerHTML = `0${i+1}`;
+        index = i;
+      }
+      closeBtn.addEventListener("click", ()=>{
+        popup.classList.toggle('active');
+      })
+      leftArrow.addEventListener("click", ()=>{
+        if(index > 0){
+            updateImage(index - 1);
+        }
+        if(index === 0){
+            index = images.length;
+        }
+      })
+      rightArrow.addEventListener("click", ()=>{
+        if(index < images.length - 1){
+            updateImage(index + 1);
+        }
+        if(index === images.length){
+            index = 0;
+        }
+      })
+     
+
+  
